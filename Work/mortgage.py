2e73c263d5e7c13bc5610539fb9fa5d5756fa2b4
Edward Andrews-Hodgson <edward@aldreth.com>
2024-03-28 15:36:08 +0000

    Complete exercise 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,19 +8,24 @@ payment = 2684.11
 total_paid = 0.0
 months = 0
 
-extra_payment_start_month = int(input('Which month should extra payment start in?'))
-extra_payment_end_month = int(input('Which month should extra payment end in?'))
-extra_payment = int(input('How much extra payment per month can you afford?'))
+extra_payment_start_month = int(input('Which month should extra payment start in?').strip() or "61")
+extra_payment_end_month = int(input('Which month should extra payment end in?').strip() or "109")
+extra_payment = int(input('How much extra payment per month can you afford?').strip() or "1000")
 
 while principal > 0:
     months = months + 1
-    if months > extra_payment_start_month and months <= extra_payment_end_month:
-        principal = principal * (1+rate/12) - payment - extra_payment
-    else:
-        principal = principal * (1+rate/12) - payment
-      
-    total_paid = total_paid + payment
-    print(months, total_paid, principal)
+    this_months_payment = payment
 
-print('Total paid', total_paid)
+    if months > extra_payment_start_month and months <= extra_payment_end_month:
+        this_months_payment = payment + extra_payment
+    
+    if this_months_payment > principal * (1+rate/12):
+      this_months_payment = principal * (1+rate/12)
+
+    principal = principal * (1+rate/12) - this_months_payment
+      
+    total_paid = total_paid + this_months_payment
+    print(months, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
 print('Months', months)
