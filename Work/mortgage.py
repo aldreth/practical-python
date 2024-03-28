2bf019caa10bd3d9abb2cc0dbb43e1020d5e0335
Edward Andrews-Hodgson <edward@aldreth.com>
2024-03-28 15:18:01 +0000

    Complete exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,10 +8,14 @@ payment = 2684.11
 total_paid = 0.0
 months = 0
 
+extra_payment_start_month = int(input('Which month should extra payment start in?'))
+extra_payment_end_month = int(input('Which month should extra payment end in?'))
+extra_payment = int(input('How much extra payment per month can you afford?'))
+
 while principal > 0:
     months = months + 1
-    if months <= 12:
-        principal = principal * (1+rate/12) - payment - 1000
+    if months > extra_payment_start_month and months <= extra_payment_end_month:
+        principal = principal * (1+rate/12) - payment - extra_payment
     else:
         principal = principal * (1+rate/12) - payment
       
